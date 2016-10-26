fbda24e2814493ee0bd1b9603fc307d8bd8462f4
NeatSketch <neatsketch@gmail.com>
2016-10-26 17:51:27 +0300

    A small fix

--- classes/actions/ActionBlog.class.php
+++ classes/actions/ActionBlog.class.php
@@ -431,7 +431,7 @@ class ActionBlog extends Action
 				 * Если запретили читать посты, уменьшаем число читателей блога
 				 * (в соответствии со старым механизмом бана)
 				 */
-				if ((!$aPerm['topics_read']) and $oBlogUser->getTopicPermissions()->check(Permissions::READ)) {
+				if (!($aPerm['topics_read']) and $oBlogUser->getTopicPermissions()->check(Permissions::READ)) {
                             $oBlog->setCountUser($oBlog->getCountUser()-1);
                         }
 
