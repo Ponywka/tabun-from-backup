c44dd6a9a9d79d8082efe3f126762da5fbca6eea
Andriy Kushnir (Orhideous) <me@orhideous.name>
2021-12-06 03:56:31 +0200

    Drop redundant script
    Signed-off-by: Andriy Kushnir (Orhideous) <me@orhideous.name>

--- docker/compile_locales.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cd /app/templates/skin/synio/locale/ru_RU/LC_MESSAGES
-msgfmt messages.po
-npx po2json -f jed1.x messages.po messages.json
-
-# Set 666 permissions so that files can be edited by users on the host
-chmod 666 messages.json messages.mo
